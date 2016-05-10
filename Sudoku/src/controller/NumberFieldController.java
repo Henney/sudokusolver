0c26a6fd305107629b0472f80d24d02106e4477b
Henney <thomas@henney.dk>
2016-05-10 17:34:59 +0200

    Readded NumberFieldController

---
+++ Sudoku/src/controller/NumberFieldController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import view.SudokuGridPane;
+import view.View;
+
+public class NumberFieldController<T> implements EventHandler<MouseEvent> {
+
+	SudokuGridPane sudoku;
+	int number;
+	
+	public NumberFieldController(SudokuGridPane sudoku, int number) {
+		this.sudoku = sudoku;
+		this.number = number;
+	}
+
+	@Override
+	public void handle(MouseEvent event) {
+		sudoku.inputNumber(number);
+	}
+
+}
