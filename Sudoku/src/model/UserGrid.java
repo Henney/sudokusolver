e9e8a343bbe33c2830725fec5b56b806d98022e7
Andreas From <andro.from@gmail.com>
2016-05-10 16:52:46 +0200

    Initialise all the given values

--- Sudoku/src/model/UserGrid.java
+++ Sudoku/src/model/UserGrid.java
@@ -8,11 +8,10 @@ import java.util.Set;
 
 import model.util.Pair;
 
-
 public class UserGrid {
-	
+
 	private Grid grid;
-	
+
 	private ArrayDeque<Integer>[][] rows;
 	private ArrayDeque<Integer>[][] cols;
 	private ArrayDeque<Integer>[][] boxes;
@@ -22,7 +21,7 @@ public class UserGrid {
 		rows = new ArrayDeque[grid.size()][grid.size()];
 		cols = new ArrayDeque[grid.size()][grid.size()];
 		boxes = new ArrayDeque[grid.size()][grid.size()];
-		
+
 		for (int i = 0; i < rows.length; i++) {
 			for (int j = 0; j < rows[i].length; j++) {
 				rows[i][j] = new ArrayDeque<Integer>();
@@ -30,10 +29,10 @@ public class UserGrid {
 				boxes[i][j] = new ArrayDeque<Integer>();
 			}
 		}
-		
-		for (int i = 0; i < grid.size(); i++) {
+
+		for (int i = 0; i < grid.numberOfFields(); i++) {
 			int x = grid.get(i);
-			
+
 			if (x != 0) {
 				x--;
 				rows[grid.rowFor(i)][x].add(i);
@@ -42,12 +41,12 @@ public class UserGrid {
 			}
 		}
 	}
-	
+
 	public UserGrid(Grid grid) {
 		this.grid = grid;
 		initPossibleValues();
 	}
-	
+
 	public UserGrid(int k) {
 		grid = new Grid(k);
 		initPossibleValues();
@@ -57,125 +56,117 @@ public class UserGrid {
 		grid = new Grid(input);
 		initPossibleValues();
 	}
-	
+
 	public UserGrid(Reader input) throws IOException {
 		grid = new Grid(input);
 		initPossibleValues();
 	}
-	
+
 	public int get(int row, int col) {
 		return grid.get(row, col);
 	}
-	
+
 	public int get(int i) {
 		return grid.get(i);
 	}
-	
+
 	public Pair<Set<Integer>, Set<Integer>> set(int row, int col, int val) {
-		return set(row*grid.size() + col, val);
+		return set(row * grid.size() + col, val);
 	}
-	
+
 	public Pair<Set<Integer>, Set<Integer>> set(int i, int val) {
 		int old = grid.get(i);
 		grid.set(i, val);
-		
+
 		HashSet<Integer> conflicting = new HashSet<Integer>();
 		HashSet<Integer> resolved = new HashSet<Integer>();
-		
+
 		final int row = grid.rowFor(i);
 		final int col = grid.colFor(i);
 		final int box = grid.boxFor(i);
-		
+
 		if (old != 0) {
-			System.out.println(i + ", " + val);
-			
 			old--;
-			
+
 			ArrayDeque<Integer> r = rows[row][old];
 			ArrayDeque<Integer> c = cols[col][old];
 			ArrayDeque<Integer> b = boxes[box][old];
-			
+
 			r.remove(i);
 			c.remove(i);
 			b.remove(i);
-			
+
 			if (r.size() == 1) {
-				int f = r.peek();
-				
-				if(!inConflict(f)) {
-					resolved.add(f);
+				if (!inConflict(r.peek())) {
+					resolved.add(r.peek());
 				}
 			}
-			
+
 			if (c.size() == 1) {
-				int f = c.peek();
-				
-				if(!inConflict(f)) {
-					resolved.add(f);
+				if (!inConflict(c.peek())) {
+					resolved.add(c.peek());
 				}
 			}
-			
+
 			if (b.size() == 1) {
-				int f = b.peek();
-				
-				if(!inConflict(f)) {
-					resolved.add(f);
+				if (!inConflict(b.peek())) {
+					resolved.add(b.peek());
 				}
 			}
 		}
-		
-		System.out.println(i + ", " + val + ": " + resolved);
-		
+
 		if (val == 0) {
 			return new Pair<Set<Integer>, Set<Integer>>(conflicting, resolved);
 		}
-		
-		ArrayDeque<Integer> r = rows[row][val-1];
+
+		val--;
+
+		ArrayDeque<Integer> r = rows[row][val];
+		ArrayDeque<Integer> c = cols[col][val];
+		ArrayDeque<Integer> b = boxes[box][val];
+
+		System.out.println(val + ": " + r);
+
 		r.push(i);
 		if (r.size() > 1) {
 			conflicting.addAll(r);
 		}
 
-		ArrayDeque<Integer> c = cols[col][val-1];
 		c.push(i);
 		if (c.size() > 1) {
 			conflicting.addAll(c);
 		}
-		
-		ArrayDeque<Integer> b = boxes[box][val-1];
+
 		b.push(i);
 		if (b.size() > 1) {
 			conflicting.addAll(b);
 		}
-			
+
 		return new Pair<Set<Integer>, Set<Integer>>(conflicting, resolved);
 	}
-	
+
 	private boolean inConflict(int i) {
-		final int row = grid.rowFor(i);
-		final int col = grid.colFor(i);
-		final int box = grid.boxFor(i);
-		
 		int x = grid.get(i);
-		
+
 		if (x == 0) {
 			return false;
 		}
-		
+
 		x--;
-		
-		return rows[row][x].size() > 1 || cols[col][x].size() > 1 || boxes[box][x].size() > 1;
+
+		return rows[grid.rowFor(i)][x].size() > 1 || cols[grid.colFor(i)][x].size() > 1
+				|| boxes[grid.boxFor(i)][x].size() > 1;
 
 	}
-	
+
 	public int k() {
 		return grid.k();
 	}
-	
+
 	public int size() {
 		return grid.size();
 	}
-	
+
 	public Grid getGrid() {
 		return grid;
 	}
--- Sudoku/src/model/tests/UserGridTest.java
+++ Sudoku/src/model/tests/UserGridTest.java
@@ -2,6 +2,8 @@ package model.tests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Set;
 
 import org.junit.Test;
@@ -26,11 +28,26 @@ public class UserGridTest {
 		assertTrue(s21.snd.isEmpty());
 		
 		Pair<Set<Integer>, Set<Integer>> s23 = grid.set(2, 3);
-
-		System.out.println(s23.snd);
 		
 		assertTrue(s23.fst.isEmpty());
 		assertTrue(s23.snd.contains(0));
+	}
+	
+	@Test
+	public void box9regression() throws IOException {
+		UserGrid grid = new UserGrid(new FileReader("puzzles/sudoku1.txt"));
+		
+		int other = 3*9;
+		assertTrue(grid.get(other) == 9);
+		
+		int i = 3*9 + 4;
+		assertTrue(grid.getGrid().rowFor(other) == grid.getGrid().rowFor(i));
+		
+		Pair<Set<Integer>, Set<Integer>> s34 = grid.set(i, 9);
+		
+		System.out.println(s34.fst);
+		
+		assertTrue(s34.fst.contains(other));
 	}
 
 }
