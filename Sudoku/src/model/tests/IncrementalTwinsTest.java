39b21651382c563b2817418d74574d63e312198c
Andreas From <andro.from@gmail.com>
2016-06-12 12:19:18 +0200

    Much better (Incremental)Twins tests

--- Sudoku/src/model/tests/IncrementalTwinsTest.java
+++ Sudoku/src/model/tests/IncrementalTwinsTest.java
@@ -15,7 +15,7 @@ import model.util.IntPriorityQueue;
 public class IncrementalTwinsTest {
 
 	@Test
-	public void incrementalTwinsTest() throws IOException {
+	public void incrementalTwinsColTest() throws IOException {
 		String input =
 				"2\n" +
 				"1;.;.;.\n" +
@@ -39,21 +39,99 @@ public class IncrementalTwinsTest {
 		
 		IncrementalTwinsTactic tac = new IncrementalTwinsTactic(g, pGrid);
 		
+		assertTrue(pvs[9].get(3));
+		assertTrue(pvs[9].get(4));
+		
+		assertTrue(pvs[13].get(3));
+		assertTrue(pvs[13].get(4));
+		
 		tac.apply(4, 2);
 		
-		// 9 and 13, now cannot contain 3 or 4 because those have to be in the same box as the given 1, 2
+		// 9 and 13, now cannot contain 3 or 4 because the two fields above in the box with the given 1, 2 have to contain 3 and 4
 			
 		assertFalse(pvs[9].get(3));
 		assertFalse(pvs[9].get(4));
 		
 		assertFalse(pvs[13].get(3));
 		assertFalse(pvs[13].get(4));
-		
-		assertFalse(pvs[8].get(1));
-		assertFalse(pvs[8].get(2));
-		
-		assertFalse(pvs[12].get(1));
-		assertFalse(pvs[12].get(2));
 	}
 	
+	
+	@Test
+	public void incrementalTwinsRowTest() throws IOException {
+		String input =
+				"2\n" +
+				"1;2;.;.\n" +
+				".;.;.;.\n" +
+				".;.;.;.\n" +
+				".;.;.;.\n";
+		
+		Grid g = new Grid(input);
+		
+		IntPriorityQueue pq = new IntPriorityQueue(g.numberOfFields(), g.size());
+
+		PossibleValues[] pvs = g.findPossibleValues();
+
+		for (int i = 0; i < pvs.length; i++) {
+			if (pvs[i] != null) {
+				pq.insert(i, pvs[i].possible());
+			}
+		}
+		
+		PossibleValuesGrid pGrid = new PossibleValuesGrid(g, pvs, pq);
+		
+		IncrementalTwinsTactic tac = new IncrementalTwinsTactic(g, pGrid);
+		
+		assertTrue(pvs[6].get(3));
+		assertTrue(pvs[6].get(4));
+		
+		assertTrue(pvs[7].get(3));
+		assertTrue(pvs[7].get(4));
+		
+		tac.apply(1, 2);
+		
+		// 6 and 7, now cannot contain 3 or 4 because the two fields to the left in the box with the given 1, 2 have to contain 3 and 4
+			
+		assertFalse(pvs[6].get(3));
+		assertFalse(pvs[6].get(4));
+		
+		assertFalse(pvs[7].get(3));
+		assertFalse(pvs[7].get(4));
+	}
+	
+	@Test
+	public void incrementalTwinsBoxTest() throws IOException {
+		String input =
+				"2\n" +
+				"1;.;.;.\n" +
+				".;.;.;.\n" +
+				".;2;.;.\n" +
+				".;.;.;.\n";
+		
+		Grid g = new Grid(input);
+		
+		IntPriorityQueue pq = new IntPriorityQueue(g.numberOfFields(), g.size());
+
+		PossibleValues[] pvs = g.findPossibleValues();
+
+		for (int i = 0; i < pvs.length; i++) {
+			if (pvs[i] != null) {
+				pq.insert(i, pvs[i].possible());
+			}
+		}
+		
+		PossibleValuesGrid pGrid = new PossibleValuesGrid(g, pvs, pq);
+		
+		IncrementalTwinsTactic tac = new IncrementalTwinsTactic(g, pGrid);
+		
+		assertTrue(pvs[4].get(3));
+		assertTrue(pvs[4].get(4));
+		
+		tac.apply(1, 2);
+		
+		// 4 cannot contain 3 or 4, because those have to go in 1 and 5
+			
+		assertFalse(pvs[4].get(3));
+		assertFalse(pvs[4].get(4));
+	}
 }
